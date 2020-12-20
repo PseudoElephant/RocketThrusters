e272b50060e95cb801ad5a3cfa47fca20f109a58
Alessandro Pisa <aleppaulon@gmail.com>
2020-12-19 22:30:00 -0600

    Added Basic Saw Behavior as Well as Sprites

---
+++ Assets/Scripts/SawBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBehaviour : MonoBehaviour
+{
+    public bool rotateRight;
+
+    [Range(0f, 10f)]
+    public float rotationSpeed;
+
+    private bool prevRotateRight;
+    private float multiplier = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevRotateRight = rotateRight;
+
+        if (rotateRight)
+        {
+            multiplier *= -1;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rotateRight != prevRotateRight)
+        {
+            prevRotateRight = rotateRight;
+            multiplier *= -1;
+        }
+
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime * multiplier, Space.Self);
+    }
+}
---
+++ Assets/Scripts/ShooterBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterBehaviour : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    private Transform targetTransform;
+    [SerializeField, Range(0f,10f)] float shootSpeed;
+    [SerializeField] float treshHoldAngle;
+    private Transform parentTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Shoot", 0, shootSpeed);
+        targetTransform = target.GetComponent<Transform>();
+        parentTransform = GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float angleToLookAt = Mathf.Atan(targetTransform.position.x - parentTransform.position.x / targetTransform.position.y - parentTransform.position.y);
+        float angleAdd = (transform.rotation.z + angleToLookAt) / 2f;
+        transform.Rotate(0, 0, angleAdd * Time.deltaTime * 100, Space.Self);
+    }
+
+    void Shoot()
+    {
+        
+    }
+}
