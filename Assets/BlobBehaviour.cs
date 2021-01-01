1dd5bfcf5e6bc98a933eb09f0a632992f4fcf50b
AlePPisa <aleppaulon@gmail.com>
2020-12-31 18:45:14 -0600

    Attempted to fix blob atack

--- Assets/BlobBehaviour.cs
+++ Assets/BlobBehaviour.cs
@@ -6,18 +6,13 @@ using Utility;
 public class BlobBehaviour : MonoBehaviour
 {
     public int attackRadius;
-
     public GameObject target;
-
     public float speed;
-
+    
     private JelloBody _body;
-
-    private bool grounded = false;
-
-    // Cache
+    private bool _grounded = false;
     private Rigidbody2D _rigidbody2D;
-    // Start is called before the first frame update
+    
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -29,41 +24,42 @@ public class BlobBehaviour : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        grounded = false;
+        // _grounded = false;
         //find the direction from the point masses curerrent position to its respective xformed base shape position.
-   
-
     }
 
     IEnumerator Attack()
     {
         while (true)
         {
-
-            yield return new WaitForSeconds(1);
-            Vector2 direction = (target.transform.position - transform.position).normalized;
-
-
-            _body.AddForce(direction * speed);
+            if (_grounded)
+            {
+                yield return new WaitForSeconds(1);
+                Vector2 direction = (target.transform.position - transform.position).normalized;
+                _body.AddForce(direction * speed);
+                _grounded = false;
+            }
+            else
+            {
+                yield return new WaitForSeconds(1);
+            }
+            
             //_body.AddForce(direction * speed, _body.getInternalPointMass(0).Position, true);
-
             //_body.getInternalPointMass(0);
         }
     }
     private void ProcessCollisionEvent(JelloCollision jelloCollision)
     {
-        //loop throug each contact in the collision.
-        for (int i = 0; i < jelloCollision.contacts.Length; i++)
-        {
-
-            //grounded logic
-            if (!grounded)
+        //grounded logic
+        if(!_grounded) {
+            //loop through each contact in the collision.
+            for (int i = 0; i < jelloCollision.contacts.Length; i++)
             {
                 //if the hit point of this contact is below the center of the body, count as grounded.
                 //This works well for the JelloCharacter in the SpiffyDemoScene but may not be the best for all cases.
                 if (jelloCollision.contacts[i].hitPoint.y < transform.position.y)
                 {
-                    grounded = true;
+                    _grounded = true;
                 }
             }
         }
