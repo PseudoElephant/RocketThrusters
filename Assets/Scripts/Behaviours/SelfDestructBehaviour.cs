f7a301aef01e37fdab9c4d08dd820f0190e524a0
AlePPisa <aleppaulon@gmail.com>
2021-01-02 22:37:52 -0600

    Started a Level

---
+++ Assets/Scripts/Behaviours/SelfDestructBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructBehaviour : MonoBehaviour
+{
+    public bool useTime;
+    public float selfDestructTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (useTime)
+        {
+            StartCoroutine(DestroySelf());
+        }
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSecondsRealtime(selfDestructTime);
+        Destroy(gameObject);
+    }
+
+    public void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+}
