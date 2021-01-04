239c60313902d4152f6e4b73a42018359eeeb363
Stephan Guingor <stephan.guingor04@gmail.com>
2021-01-04 00:57:23 -0600

    Camera Sexy

---
+++ Assets/CameraAnimatorBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraAnimatorBehaviour : MonoBehaviour
+{
+    // Parameters
+    private Animator _animator;
+    public CinemachineStateDrivenCamera _stateCamera;
+    public CinemachineVirtualCamera _zoomCamera;
+    public CinemachineVirtualCamera _defaultCamera;
+
+    // Constants
+    private const string Zoom = "isZoom";
+    private static readonly int IsZoom = Animator.StringToHash(Zoom);
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void ZoomIn(int zoom)
+    {
+        if (!_animator.GetBool(IsZoom))
+        {
+            //_zoomCamera.m_Lens.OrthographicSize = zoom;   
+            _animator.SetBool(IsZoom,true);
+           // StartCoroutine(SwapToDefault());
+          
+        }
+        else
+        {
+         //   StopAllCoroutines();
+         //   _defaultCamera.m_Lens.OrthographicSize = zoom;
+            _animator.SetBool(IsZoom,false);
+        }
+            
+    }
+
+    IEnumerator Check()
+    {
+        yield return new WaitForSeconds(1);
+        _zoomCamera.m_Lens.OrthographicSize = 1;
+    }
+    IEnumerator SwapToDefault()
+    {
+     
+        yield return new WaitForSeconds(_stateCamera.m_DefaultBlend.BlendTime+1f);
+        _defaultCamera.m_Lens.OrthographicSize = _zoomCamera.m_Lens.OrthographicSize;
+        _animator.SetBool(IsZoom,false);
+    }
+
+}
