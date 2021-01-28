eb56bdc955a5723ff74782e7f2b1d43ecd8d1524
AlePPisa <aleppaulon@gmail.com>
2021-01-27 21:44:06 -0600

    Added animations and buttons to main menu

---
+++ Assets/MenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : Button
+{
+    private void OnMouseOver()
+    {
+        Debug.Log("Check?");
+    }
+}
